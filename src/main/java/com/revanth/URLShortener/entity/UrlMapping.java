2a4a38e8b581a4cea21f0970d3ff005d6510802b
29REV <24149056@drngpit.ac.in>
2026-09-21 12:23:06 +0530

    completed with entity mapping

---
+++ src/main/java/com/revanth/URLShortener/entity/UrlMapping.java
@@ -0,0 +1,31 @@
+package com.revanth.URLShortener.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "url_mappings")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
--- src/main/java/com/revanth/URLShortener/repository/URLmappingrepository.java
+++ src/main/java/com/revanth/URLShortener/repository/URLmappingrepository.java
@@ -1,5 +1,9 @@
 package com.revanth.URLShortener.repository;
 
-public class URLmappingrepository {
-    
+import com.revanth.URLShortener.entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
 }
