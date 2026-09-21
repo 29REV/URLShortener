cddbbcb6f6d82f7f43e3e387059da81a0ac2f7c5
29REV <24149056@drngpit.ac.in>
2026-09-21 11:21:50 +0530

    Initial push

---
+++ src/test/java/com/revanth/URLShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revanth.URLShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
